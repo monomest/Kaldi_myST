06f3fb02c524112cbd905367c2d5757eece36a6a
monomest <reneeylu29@gmail.com>
2020-02-17 12:55:58 +1100

    Prelim fixes for labels and tags

--- s5/local/myst_getchars.sh
+++ s5/local/myst_getchars.sh
@@ -10,28 +10,47 @@ FILE=$MYST_ROOT/$FILE
 
 UCHAR=local/uniqchar.txt
 TRANS=local/trans.txt
+TRANS_LM=local/trans_lm.txt
 TAGS=local/tags.txt
 touch $UCHAR
 touch $TRANS
+touch $TRANS_LM
 touch $TAGS
 
 #sed -i "s/[[:space:]]\+/ /g" $FILE
 
-while read line; do
-	trans=$(cut -d ' ' -f2- <<< "$line")
-	echo "$trans" >> $TRANS
-done < $FILE
+#echo "Creating local/trans.txt..."
+#while read line; do
+#	trans=$(cut -d ' ' -f2- <<< "$line")
+#	echo "$trans" >> $TRANS
+#done < $FILE
 
+#echo "Creating local/uniqchar.txt..."
 # Get all the unique characters
-cat $TRANS | fold -w1 | sort -u > $UCHAR
+#cat $TRANS | fold -w1 | sort -u > $UCHAR
 
+echo "Creating local/tags.txt..."
 # Get all the tags and labels for < >
-grep -o "<[[:graph:]]*.>" $TRANS | sort -u >> $TAGS
+echo "Getting the tags < >"
+grep -o "<[^>]*>" $TRANS | sort -u >> $TAGS
 # ( )
-grep -o "([[:graph:]]*.)" $TRANS | sort -u >> $TAGS
+echo "Getting the tags ( )"
+grep -o "([^)]*)" $TRANS | sort -u >> $TAGS
 # []
-grep -o "\[[[:graph:]]*.\]" $TRANS | sort -u >> $TAGS
+echo "Getting the tags [ ]"
+grep -o "\[[^]]*\]" $TRANS | sort -u >> $TAGS
 # + +
-grep -o "\+[[:graph:]]*+" $TRANS | sort -u >> $TAGS
+echo "Getting the tags + +"
+grep -o "\+[^+]*+" $TRANS | sort -u >> $TAGS
+# * *
+echo "Getting the tags * *"
+grep -o "\*[[:graph:]]*\*" $TRANS | sort -u >> $TAGS
 
+echo "Sorting..."
 sort -o -u $TAGS
+
+# Remove all the tags and event labels
+echo "Removing all tags and event labels..."
+sed 's/<[^>]*>/ /g; s/([^)]*)/ /g; s/\[[^]]*\]/ /g; s/\+[^+]*+/ /g; s/\*[[:graph:]]*\*/ /g;' $TRANS > $TRANS_LM
+
+echo "DONE"
